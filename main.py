2d60a8c0a9417cfe229ec4ef2e66c4b0e5894414
TeamMasse <admin@thefischer.org>
2026-05-21 19:54:28 +0200

    customizations

--- main.py
+++ main.py
@@ -70,9 +70,9 @@ def info() -> typing.Dict:
     return {
         "apiversion": "1",
         "author": "Gruppe 6",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "color": "#008CFF",  # TODO: Choose color
+        "head": "train",  # TODO: Choose head
+        "tail": "nr-booster",  # TODO: Choose tail
     }
 
 
